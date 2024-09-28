4f8904e3d48bd2d9482970d905e648fb0f20f231
rtaydev <russ.taylor.48@icloud.com>
2024-09-28 07:05:29 +0100

    fix: Persistant gate import

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import '../msw.polyfills';
 import { server } from '../mocks/server';
 import { Provider } from 'react-redux';
 import { store, persistor } from '../store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 server.listen();
 
--- app/customer-details.tsx
+++ app/customer-details.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchBookings } from './bookingsSlice';
+import { fetchBookings } from '../store/slices/bookingSlice';
 import { View, Text, ActivityIndicator } from 'react-native';
 
 export default function CustomerDetailsScreen() {
