bf7b53ba6e35456a4d44082768531682eccb2336
rtaydev <russ.taylor.48@icloud.com>
2024-09-29 08:19:53 +0100

    fix: Fix dark mode

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,14 +5,19 @@ import { Provider } from "react-redux";
 import { store, persistor } from "../store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useColorScheme } from "react-native";
+import { Colors } from "@/constants/Colors";
 
 server.listen();
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const backgroundColor = Colors[colorScheme ?? "light"].background;
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <SafeAreaView style={{ flex: 1 }}>
+        <SafeAreaView style={{ flex: 1, backgroundColor }}>
           <Slot />
         </SafeAreaView>
       </PersistGate>
